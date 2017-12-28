664947dfaaaa619046fd3ab3b9d144e88068a79d
Jester <jester.dash@gmail.com>
2017-12-28 11:57:47 +0200

    Add handler for dev and prod (#1)
    * Add handler for dev and prod
    
    * Fixes, related to commetns

--- handler.js
+++ handler.js
@@ -1,16 +1,50 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+const IncomingWebhook = require('@slack/client').IncomingWebhook;
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({'accessKeyId': process.env.aws_key_id, 'secretAccessKey': process.env.aws_secret_key});
+
+const slackHookUrl = process.env.slack_hook_url;
+const bucketName   = process.env.bucket_name;
+
+module.exports.handler = (event, context, callback) => {
+  let message = event.Records[0].ses.mail;
+
+  s3.getObject({
+    Bucket: bucketName,
+    Key: message.messageId
+  }, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      callback(err);
+    } else {
+      if (message.commonHeaders.from[0].includes('alerts@tracelyticsmail.com')) {
+        handleReceived(message.commonHeaders.subject, data.Body, callback);
+      } else {
+        callback(`Unknown email sender: ${message.from[0]}`);
+      }
+    };
+  });
+};
+
+function handleReceived(subject, message, callback) {
+  const emailArray = String(message).split('\n');
+  const appIndex = emailArray.findIndex(item => item === 'App:\r');
+  let slackMessage;
+
+  if (appIndex !== -1) {
+    slackMessage = `${subject}\n Application: ${emailArray[appIndex + 2].replace(/\*\*/g, '')} Value: ${emailArray[appIndex + 10].replace(/\*\*/g, '')}`;
+  } else {
+    slackMessage = `${subject}. Additional info is not available - check email template.`;
   };
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+  const webhook = new IncomingWebhook(slackHookUrl);
+  webhook.send(slackMessage, function(err, header, statusCode) {
+    if (err) {
+      console.log('Error posting message to Slack API:', err);
+    } else {
+      console.log('Message posted successfully', statusCode);
+    }
+  });
+  callback(null, null);
+}
